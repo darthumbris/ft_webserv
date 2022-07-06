023af3235327e1ddc1ac0d1b0697653edf429f5d
Siebe Hoogenboom <shoogenb@student.codam.nl>
2022-07-06 16:47:12 +0200

    reorderd some functions and variables

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -23,13 +23,13 @@ class Server
 		Server & operator=(const Server &assign);
 
 		// Getters
-		std::string							getServerIp() const;
 		int									getServerPort() const;
-		std::vector<std::string>			getServerNames() const;
-		std::map<std::string, Location*>	getLocationMap() const;
-		std::string							getErrorPage() const;
 		int									getClientBodySize() const;
 		int									getServerSocket() const;
+		std::string							getServerIp() const;
+		std::string							getErrorPage() const;
+		std::vector<std::string>			getServerNames() const;
+		std::map<std::string, Location*>	getLocationMap() const;
 
 		// Setters
 		void	setServerIp(std::string ip);
@@ -47,9 +47,9 @@ class Server
 		int									_client_body_size;
 		int									_server_fd;
 		int									_server_port;
-		std::vector<std::string>			_server_name;
 		std::string							_server_ip;
 		std::string							_error_page;
+		std::vector<std::string>			_server_name;
 		std::map<std::string, Location*>	_location;
 };
 
